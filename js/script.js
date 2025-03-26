e106835b80481cc2201d657405cdfd447ba70c54
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-03-26 19:46:11 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// function multiplyArray(arr, num, callback) {
+//     const result = []
+//     for(const num of arr) {
+//         result.push(element * num)
+//     }
+//     callback(result)
+// }
+
+// function cb(newArr){
+//     console.log(`New result arrey: ${newArr}`);}
+// // multiplyArray(arr, 3, (newArr) => {console.log(`New result arrey: ${newArr}`);});
+
+// multiplyArray(arr1, 5, (newArr) => {console.log(`New result arrey: ${newArr}`)})
+
+
+// Створити стрілкову функцію hello1(), яка при визові буде вертати текст “Привіт JavaScript
+
+const hello1 = (text) => {
+    return text;
+}
+
+console.log(hello1("Привіт JavaScript"));
+
